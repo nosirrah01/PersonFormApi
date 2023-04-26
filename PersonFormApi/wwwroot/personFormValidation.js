5afe59e4dad0fd230c0d7340719a778b5e36dc89
Nosirrah01 <nosirrah01@gmail.com>
2023-04-26 16:45:41 -0400

    Add personFormValidation.js that makes sure all fields are filled out before submitting

---
+++ PersonFormApi/wwwroot/personFormValidation.js
@@ -0,0 +1,14 @@
+﻿const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    const age = document.querySelector('#age').value;
+    const sex = document.querySelector('input[name="sex"]:checked');
+
+    if (!name || !age || !sex) {
+        alert('Please fill out all the fields!');
+    } else {
+        form.submit();
+    }
+});
